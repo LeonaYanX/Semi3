c39b6bf338672e91df8201d1733aad11550c288a
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-11 21:43:07 +0400

    Task 2

--- Program.cs
+++ Program.cs
@@ -7,23 +7,49 @@
 
 // тип массив + [] + название = new + тип + [размер]
 
-int [] arrray = new int [8]; 
+// int [] arrray = new int [8]; 
 
-System.Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-string find= "Нет";
+// string find= "Нет";
+
+// for (int i = 0; i < arrray.Length; i++)
+// {
+//     arrray[i] = new Random().Next(0,10);
+//     Console.Write(arrray[i]+ " ");
+
+//     if(arrray[i]== num)
+//     {
+//         find = "Да";
+        
+//     }
+// }
+
+// System.Console.WriteLine(find);
+
+
+// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+int sizeArray = 10;
+
+int [] arrray = new int [sizeArray]; 
 
 for (int i = 0; i < arrray.Length; i++)
 {
-    arrray[i] = new Random().Next(0,10);
+    arrray[i] = new Random().Next(-10,11);
     Console.Write(arrray[i]+ " ");
-
-    if(arrray[i]== num)
-    {
-        find = "Да";
-        
-    }
 }
 
-System.Console.WriteLine(find);
+System.Console.WriteLine();
+
+for (int i = 0; i < arrray.Length; i++)
+{
+    arrray[i] = -1 * arrray[i];
+    Console.Write(arrray[i]+ " ");
+}
