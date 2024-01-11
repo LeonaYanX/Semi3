e8e5b030941ab7b2da5a4aec900a2f38b8e8ef3a
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-11 21:32:13 +0400

    Task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+// тип массив + [] + название = new + тип + [размер]
+
+int [] arrray = new int [8]; 
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string find= "Нет";
+
+for (int i = 0; i < arrray.Length; i++)
+{
+    arrray[i] = new Random().Next(0,10);
+    Console.Write(arrray[i]+ " ");
+
+    if(arrray[i]== num)
+    {
+        find = "Да";
+        
+    }
+}
+
+System.Console.WriteLine(find);
